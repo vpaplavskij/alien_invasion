1a2d3234a23382ffb2db7868828857da8120bea1
vpaplavskij <ccoderjoe@gmail.com>
2021-09-16 00:09:38 +0300

    first commit

---
+++ settings.py
@@ -0,0 +1,36 @@
+class Settings:
+
+    def __init__(self):
+        # screen
+        self.screen_width = 1200
+        self.screen_height = 1000
+        self.bg_color = (230, 230, 230)
+
+        # ship
+        self.ship_limit = 3
+        # bullet
+        self.bullet_width = 3
+        self.bullet_height = 10
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+        # alien
+        self.fleet_drop_speed = 10
+        # game
+        self.speedup_scale = 1.1
+        self.score_scale = 1.25
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 1.5
+        self.bullet_speed = 2.0
+        self.alien_speed = 0.6
+        self.fleet_direction = 1
+        self.alien_points = 50
+
+    def increase_speed(self):
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
+
+        self.alien_points = int(self.alien_points * self.score_scale)
+
